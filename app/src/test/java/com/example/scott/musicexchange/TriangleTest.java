5ece37f6ba1cf068282c6ce6fd6868fb979d86bb
Scott Murray <rai.five@gmail.com>
2017-10-30 08:23:45 +0000

    Final Push

--- app/src/test/java/com/example/scott/musicexchange/TriangleTest.java
+++ app/src/test/java/com/example/scott/musicexchange/TriangleTest.java
@@ -1,8 +1,29 @@
 package com.example.scott.musicexchange;
 
+import com.example.scott.musicexchange.Instruments.Triangle;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by scott on 30/10/2017.
  */
 
 public class TriangleTest {
+
+    Triangle triangle;
+
+    @Before
+    public void before(){
+        triangle = new Triangle("T27", "Master", "T2", 250, 290, "Titanium");
+
+    }
+
+
+    @Test
+    public void canCalcMarkup(){
+        assertEquals(40, triangle.calculateMarkup(), 0.01);
+    }
 }
